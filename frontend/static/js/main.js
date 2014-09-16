757ae178b344a59295b68c329f61306628d61001
Wouter <tuvokki@gmail.com>
2014-09-16 22:06:33 +0200

    Log geolocation errors as error

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -34,16 +34,16 @@ function showPosition(position) {
 function showError(error) {
   switch(error.code) {
     case error.PERMISSION_DENIED:
-      console.log("User denied the request for Geolocation.");
+      console.error("User denied the request for Geolocation.");
       break;
     case error.POSITION_UNAVAILABLE:
-     console.log("Location information is unavailable.");
+     console.error("Location information is unavailable.");
       break;
     case error.TIMEOUT:
-      console.log("The request to get user location timed out.");
+      console.error("The request to get user location timed out.");
       break;
     case error.UNKNOWN_ERROR:
-      console.log("An unknown error occurred.");
+      console.error("An unknown error occurred.");
       break;
   }
 }
