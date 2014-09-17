ba25141cfbe5e181aa3cadaa98b2c2251008715c
Wouter <tuvokki@gmail.com>
2014-09-17 14:41:07 +0200

    Added some taps to the inserts test

--- test/insert-test.js
+++ test/insert-test.js
@@ -42,9 +42,49 @@ var locations =
       description: dimsum.sentence(2),
       geolocation: [51.8358791,5.8855854]
     },{
-      name: 'Kitty',
+      name: 'Huidverzorgingsstudio Kitty',
       description: dimsum.sentence(2),
       geolocation: [51.8332951,5.8865869]
+    },{
+      name: 'Tati',
+      description: dimsum.sentence(2),
+      geolocation: [51.834705,5.8765441]
+    },{
+      name: 'in de Kazerne',
+      description: dimsum.sentence(2),
+      geolocation: [51.834884,5.8767372]
+    },{
+      name: 'Restaurant Vesters',
+      description: dimsum.sentence(2),
+      geolocation: [51.83562,5.8745001]
+    },{
+      name: 'global giftshop',
+      description: dimsum.sentence(2),
+      geolocation: [51.8347615,5.8679447]
+    },{
+      name: 'Blixem',
+      description: dimsum.sentence(2),
+      geolocation: [51.8365382,5.8691356]
+    },{
+      name: 'De Knollentuin',
+      description: dimsum.sentence(2),
+      geolocation: [51.8369757,5.8666036]
+    },{
+      name: 'Banketbakkerij Pruyn',
+      description: dimsum.sentence(2),
+      geolocation: [51.8360808,5.8592115]
+    },{
+      name: 'We gaan beginnen',
+      description: dimsum.sentence(2),
+      geolocation: [51.8360808,5.8592115]
+    },{
+      name: 'Café St. Anneke',
+      description: dimsum.sentence(2),
+      geolocation: [51.8384143,5.8597586]
+    },{
+      name: 'Café Maxim',
+      description: dimsum.sentence(2),
+      geolocation: [51.8394816,5.8555851]
     }
   ];
 
@@ -57,22 +97,23 @@ describe('Array insert test', function(){
       console.log("[DISABLED] This test is disabled. Use for inserting a basic dataset with 7 tap-points.");
     done();
   });
-  // locations.forEach(function(element, index, array) {
-  //   it('responds with a json success message', function(done){
-  //     request(app)
-  //     .post(api_uri)
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .send({'description': element.description, 'name': element.name, 'geolocation': element.geolocation})
-  //     .expect(200)
-  //     .end(function(err, res) {
-  //       should.not.exist(err);
-  //       res.body.data.name.should.eql(element.name);
-  //       res.body.data.description.should.eql(element.description);
-  //       res.body.data.geolocation.should.eql(element.geolocation);
-  //       console.log("[PASSED] Inserted id(%s), result of POST:\n %s", res.body.data._id, JSON.stringify(res.body.data, null, "  "));
-  //       done();
-  //     });
-  //   });
-  // });
+
+  locations.forEach(function(element, index, array) {
+    it('inserts all taps in this locations variable', function(done){
+      request(app)
+      .post(api_uri)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .send({'description': element.description, 'name': element.name, 'geolocation': element.geolocation})
+      .expect(200)
+      .end(function(err, res) {
+        should.not.exist(err);
+        res.body.data.name.should.eql(element.name);
+        res.body.data.description.should.eql(element.description);
+        res.body.data.geolocation.should.eql(element.geolocation);
+        console.log("[PASSED] Inserted id(%s), result of POST:\n %s", res.body.data._id, JSON.stringify(res.body.data, null, "  "));
+        done();
+      });
+    });
+  });
 });
