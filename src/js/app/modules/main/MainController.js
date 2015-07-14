1a512b25b9e66dc99cba640a95306fee7ccb67ac
elger <elgervb@gmail.com>
2015-07-14 21:09:52 +0200

    add the google map

--- src/js/app/modules/main/MainController.js
+++ src/js/app/modules/main/MainController.js
@@ -3,4 +3,23 @@
  */
 app.controller('MainController', ['$scope', function($scope) {
 
+  var mapOptions = {
+    center: new google.maps.LatLng(51.84520017, 5.83402263),
+    zoom: 16,
+    panControl: false,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.LARGE,
+      position: google.maps.ControlPosition.LEFT_CENTER
+    },
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    },
+    streetViewControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP // Or: google.maps.MapTypeId.TERRAIN
+  }
+
+  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
 }]);
