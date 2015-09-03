7a25532c9d62ab439122f4cad93084aa2263802d
Wouter Roosendaal <tuvokki@gmail.com>
2015-09-03 10:48:45 +0200

    Added user REST api

--- config/routes.js
+++ config/routes.js
@@ -6,17 +6,20 @@ exports.default = {
       routes.js 
       ---------------------- */
       get: [
-        { path: '/point', action: 'pointsList' },       // (GET) /api/point
-        { path: '/point/:name', action: 'pointView' },  // (GET) /api/point/theo-there-is-no-point
+        { path: '/point', action: 'pointsList' },         // (GET) /api/point
+        { path: '/point/:name', action: 'pointView' },    // (GET) /api/point/theo-there-is-no-point
+        { path: '/user', action: 'usersList' },           // (GET) /api/user
       ],
       post: [
-        { path: '/point', action: 'pointAdd' },         // (POST) /api/point
+        { path: '/point', action: 'pointAdd' },           // (POST) /api/point
+        { path: '/user', action: 'userAdd' },             // (POST) /api/user
       ],
       put: [
-        { path: '/point/:name', action: 'pointEdit' },   // (PUT) /api/point/theo-there-is-no-point
+        { path: '/point/:name', action: 'pointEdit' },    // (PUT) /api/point/theo-there-is-no-point
       ],
       delete: [
-        { path: '/point/:name', action: 'pointDelete' },// (DELETE) /api/point/theo-there-is-no-point
+        { path: '/point/:name', action: 'pointDelete' },  // (DELETE) /api/point/theo-there-is-no-point
+        { path: '/user/:name', action: 'userDelete' },    // (DELETE) /api/user/theo
       ]
       /*
       all: [
